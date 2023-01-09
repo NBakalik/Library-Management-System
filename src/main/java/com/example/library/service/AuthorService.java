e3610c641292d91ee4fed0e3b2d6b0f8d1d24730
NBakalik <bakalik.nazariy@gmail.com>
2023-01-09 17:08:12 +0200

    added methods to service classes

--- src/main/java/com/example/library/service/AuthorService.java
+++ src/main/java/com/example/library/service/AuthorService.java
@@ -1,9 +1,11 @@
 package com.example.library.service;
 
+import com.example.library.entity.Author;
 import com.example.library.repo.AuthorRepository;
-import com.example.library.repo.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuthorService {
@@ -13,5 +15,24 @@ public class AuthorService {
     @Autowired
     public AuthorService(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
+    }
+
+    public List<Author> getAllAuthor() {
+        return (List<Author>) authorRepository.findAll();
+    }
+
+    public Author getAuthor(Integer id) {
+        return authorRepository.findById(id).orElse(null);
+    }
+
+    public Author updateAuthor(Author author) {
+        authorRepository.save(author);
+        return author;
+    }
+
+    public Author deleteAuthor(Integer id) {
+        Author author = authorRepository.findById(id).orElse(null);
+        authorRepository.deleteById(id);
+        return author;
     }
 }
--- src/main/java/com/example/library/service/BookService.java
+++ src/main/java/com/example/library/service/BookService.java
@@ -1,8 +1,11 @@
 package com.example.library.service;
 
+import com.example.library.entity.Book;
 import com.example.library.repo.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
@@ -11,6 +14,25 @@ public class BookService {
     @Autowired
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBook() {
+        return (List<Book>) bookRepository.findAll();
+    }
+
+    public Book getBook(Integer id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public Book updateBook(Book book) {
+        bookRepository.save(book);
+        return book;
+    }
+
+    public Book deleteBook(Integer id) {
+        Book book = bookRepository.findById(id).orElse(null);
+        bookRepository.deleteById(id);
+        return book;
     }
 
 }
