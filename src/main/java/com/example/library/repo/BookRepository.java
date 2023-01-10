de9112396e6c60c52dd8eb43ee34fa8c704a8582
NBakalik <bakalik.nazariy@gmail.com>
2023-01-10 13:33:07 +0200

    updated repos

--- src/main/java/com/example/library/repo/BookRepository.java
+++ src/main/java/com/example/library/repo/BookRepository.java
@@ -1,9 +1,13 @@
 package com.example.library.repo;
 
 import com.example.library.entity.Book;
+import com.example.library.entity.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
+    List<Book> findBooksByCategory(Category category);
 }
