e6fce4af5d5f69ba6502e495888311fc06639581
NBakalik <bakalik.nazariy@gmail.com>
2023-01-11 15:19:08 +0200

    added all methods to Category controller

--- src/main/java/com/example/library/controller/CategoryController.java
+++ src/main/java/com/example/library/controller/CategoryController.java
@@ -5,10 +5,7 @@ import com.example.library.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,5 +37,36 @@ public class CategoryController {
         } else {
             return new ResponseEntity<>(category, HttpStatus.OK);
         }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        return new ResponseEntity<>(categoryService.addCategory(category), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable("id") int id, @RequestBody Category newCategory) {
+        if (categoryService.getCategory(id) != null) {
+            newCategory.setId(id);
+            return new ResponseEntity<>(categoryService.updateCategory(newCategory), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteCategory(@PathVariable("id") int id) {
+        Category category = categoryService.deleteCategory(id);
+        if (category != null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("")
+    public ResponseEntity<HttpStatus> deleteAllTutorials() {
+        categoryService.deleteAll();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/example/library/service/CategoryService.java
+++ src/main/java/com/example/library/service/CategoryService.java
@@ -16,6 +16,10 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    public Category addCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
     public List<Category> getAllCategory() {
         return (List<Category>) categoryRepository.findAll();
     }
@@ -24,14 +28,17 @@ public class CategoryService {
         return categoryRepository.findById(id).orElse(null);
     }
 
-    public Category updateBook(Category category) {
-        categoryRepository.save(category);
-        return category;
+    public Category updateCategory(Category category) {
+        return categoryRepository.save(category);
     }
 
     public Category deleteCategory(Integer id) {
         Category category = categoryRepository.findById(id).orElse(null);
         categoryRepository.deleteById(id);
         return category;
+    }
+
+    public void deleteAll() {
+        categoryRepository.deleteAll();
     }
 }
