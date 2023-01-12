1451f24740668752a5283871afca5c83a15a3289
NBakalik <bakalik.nazariy@gmail.com>
2023-01-12 21:07:35 +0200

    added methods to repo

--- src/main/java/com/example/library/repo/BookRepository.java
+++ src/main/java/com/example/library/repo/BookRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
     List<Book> findBooksByCategory(Category category);
+
+    List<Book> findBooksByAuthorsId(int id);
 }
