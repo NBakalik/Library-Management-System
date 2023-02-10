3b73e13ec7281dfe7c2c6133195833e38325bd97
NBakalik <bakalik.nazariy@gmail.com>
2023-02-10 18:05:21 +0200

    created UserDetailsServiceImpl class

---
+++ src/main/java/com/example/library/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.library.service;
+
+import com.example.library.entity.User;
+import com.example.library.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
