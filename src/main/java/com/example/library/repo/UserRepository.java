e1b333e850d051a0e4fd7006dc805feec6a7160d
NBakalik <bakalik.nazariy@gmail.com>
2023-02-10 18:06:10 +0200

    added method to find user by username

--- src/main/java/com/example/library/repo/UserRepository.java
+++ src/main/java/com/example/library/repo/UserRepository.java
@@ -4,7 +4,10 @@ import com.example.library.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByEmail(String email);
+    Optional<User> findByUsername(String email);
 }
